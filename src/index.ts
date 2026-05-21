2b2d3576edd9d36f668e0a1626939be81fda08e4
fofx <identityofsine@gmail.com>
2026-05-20 22:18:29 -0400

    (fix) fix for swc / babel and isolatedModules:true

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./util";
 export * from "./types/form.types";
-export { ValidatorFn } from './types/validator.types';
+export type { ValidatorFn } from './types/validator.types';
 export * from "./react/";
 export * from "./form";
 export * from "./form/functional";
